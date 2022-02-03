f668baf78b5bbd636408e70c478a4ab02e203f0c
hibah321 <hbhshahbaz321@gmail.com>
2022-02-03 23:11:46 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+
+app.set('view engine', 'ejs');
+
+
+//  listening to requests
+app.listen(3000)
+
+app.use(express.static('public'));
+
+app.get("/" , (req, res) => {
+    res.render('home', {title: 'Home'})
+});
