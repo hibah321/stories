4377259da043262f119151ee8a5b00c4c8db3cd0
hibah321 <hbhshahbaz321@gmail.com>
2022-05-30 16:57:14 +0400

    d

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const mongoURL = "mongodb+srv://hibah:Dunkkin321!@stories.kgu5k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const Story = require('./models/blog.js')
-
+const keys = require('./config/keys.js')
+const passport = require('./config/passport-setup.js')
+const router = require('./routes/storiesRouter')
+const authRouter = require('./routes/authRouter')
 
 
 // setting up express app
@@ -11,7 +12,7 @@ const app = express()
 
 //  setting up the database and listening to the server
 
-mongoose.connect(mongoURL, {useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(keys.mongodb.dbURI, {useNewUrlParser: true , useUnifiedTopology: true})
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err))
 
@@ -23,53 +24,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-// route to the home page
+// routes 
 
-app.get("/" , (req, res) => {
-    Story.find().sort({createdAt: -1})
-        .then((result) => {
-                res.render('home', {title: "Home", stories: result, style: "/styles-home.css"})
-            })
-        .catch((err) => console.log(err))
-});
-
-app.post("/", (req,res) => {
-
-    const story = new Story({
-        title: req.body.title,
-        snippet: req.body.snippet,
-        body: req.body.body
-    });
-
-
-    story.save()
-        .then(() => {
-            res.redirect("/");
-        }) 
-        .catch((err) => console.log(err))
-})
-
-
-//  route to each stories unique page
-
-app.get('/stories/:slug', (req,res) =>{
-    Story.findOne({slug: req.params.slug})
-        .then((result) => { 
-            console.log('af', result);
-            res.render('story', {title: result.title, style: "/styles-story.css", story: result})
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-        
-})
-
-// route to the create page
-
-app.get("/create", (req,res) => {
-    res.render("create", {title: "Create", style: "/styles-create.css"})
-})
-
+app.use("/", router)
+app.use("/auth", authRouter)
 
 
 
